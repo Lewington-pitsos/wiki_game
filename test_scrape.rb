6f3cf2c95e35edb9e9e1d4e16e6c8340fcd26d81
lewington <lewington@student.unimelb.edu.au>
2018-03-15 11:50:11 +1100

    added test scrape helper file

---
+++ test_scrape.rb
@@ -0,0 +1,12 @@
+require 'irbtools'
+require 'watir'
+require 'nokogiri'
+
+BR = Watir::Browser.new :firefox
+
+FLS = "//*[@class='mw-parser-output']//a[not(ancestor::table|ancestor::*[contains(@class, 'hatnote')]|ancestor::*[contains(@class, 'thumb')]|ancestor::*[contains(@class, 'IPA')]|ancestor::*[contains(@class, 'haudio')]|ancestor::*[@id='coordinates'])][not(starts-with(text(), '['))][not(contains(@class, 'image'))][starts-with(@href, '/wiki')]"
+
+def getPage
+  # returns the parsed html of the passed in browser object
+  Nokogiri::HTML(@br.html)
+end
